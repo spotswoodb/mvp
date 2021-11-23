776f89ea9cb5998920d8b345fb14d4cbe70dfb22
Ben Spotswood <spotswoodb@gmail.com>
2021-11-23 16:50:48 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import { Widget } from 'react-chat-widget'
+import 'react-chat-widget/lib/styles.css'
+
+function App() {
+
+
+
+
+  return (
+    <div className="App">
+      <Widget />
+    </div>
+  );
+}
+
+export default App;
