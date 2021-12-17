d6854956b1e53dd4bcc8e7f3be24ef3a073c2787
Benjamin <spotswoodb@gmail.com>
2021-12-17 13:59:40 -0600

    style Player Container to show cards for each player

--- src/components/players/playerContainer.js
+++ src/components/players/playerContainer.js
@@ -1,16 +1,28 @@
 import { useSelector } from 'react-redux';
+
 import { Link } from 'react-router-dom'
 
 export default function PlayerContainer() {
 
     const players = useSelector((state) => state.players)
    
+   
     return(
         <div>
             <h2>All Players</h2>
-            <ul>
-                {players.map(p => <li key={p.id}><Link to={`${p.id}/hits`}>{p.name}</Link></li>)}
-            </ul>
+                <div className="align-self-center row row-cols-1 row-cols-md-3 g-4">
+                    {players.map(p =>
+                        <div key={p.id} className="col"> 
+                            <div className="card" style={{width: '18rem'}}>
+                                <div className="card-body">
+                                    <h5 className="card-title">{p.name}</h5>
+                                    {/* <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+                                    <button type="button" className="btn btn-primary"><Link className="link-light" to={`${p.id}/hits`}>See More Stats</Link></button>
+                                </div>
+                            </div>
+                        </div>    
+                    )}
+                </div>   
         </div>
     )
 
