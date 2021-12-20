057cac33834bf1c6dac8aa20d96cb0f4f92f949b
Benjamin <spotswoodb@gmail.com>
2021-12-20 13:19:53 -0600

    console.log for form submit working, working on persisting to backend

--- src/components/players/NewPlayerForm.js
+++ src/components/players/NewPlayerForm.js
@@ -3,20 +3,49 @@ import { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { useForm } from 'react-hook-form'
 import { createPlayer } from '../../redux/PlayerActions'
-// import Dropzone from 'react-dropzone';
+
 
 export default function NewPlayerForm() {
     
-    const { register, handleSubmit, errors } = useForm();
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        defaultValues: {
+            name: "",
+            batter_id: ""
+        }
+    })
+
+    const dispatch = useDispatch()
+    
     const onSubmit = (data) => {
+        data.preventDefault()
         console.log(data)
     }
+
+    
+    
+
 
     return (
 
         <form onSubmit={handleSubmit(onSubmit)}>
-            <input {...register('name', { required: true })} placeholder="Player Name" />
-            <input {...register('batter_id', { minLength: 5 })} placeholder="Batter ID" />
+            <input {...register("name", {
+                    required: {
+                    value: true,
+                    message: "Name is required"
+                    }
+                })} 
+                name="name"
+                placeholder="Player Name" 
+                />
+            <input type="number" {...register("batter_id", {
+                    valueAsNumber: true,
+                    validate: (value) => {
+                        return value.toString().length >= 5
+                    }
+                })} 
+                name="batter_id"
+                placeholder="Batter ID"
+                />
             {errors.batter_id && <p>Batter ID must be 5 numbers or greater</p>}
             <input type="submit" />
         </form>
