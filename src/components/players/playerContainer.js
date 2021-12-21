2d332fb23179f7a8c6b560d30f70fc931cbfaa8a
Benjamin <spotswoodb@gmail.com>
2021-12-20 20:59:25 -0600

    add styling to player container

--- src/components/players/playerContainer.js
+++ src/components/players/playerContainer.js
@@ -8,20 +8,22 @@ export default function PlayerContainer() {
    
    
     return(
-        <div>
-            <h2>All Players</h2>
-                <div className="align-self-center row row-cols-1 row-cols-md-3 g-4">
-                    {players.map(p =>
-                        <div key={p.id} className="col"> 
-                            <div className="card" style={{width: '18rem'}}>
-                                <div className="card-body">
-                                    <h5 className="card-title">{p.name}</h5>
-                                    <button type="button" className="btn btn-primary"><Link className="link-light" to={`${p.id}/hits`}>See More Stats</Link></button>
+        <div className="player-container">
+            <h2 className="text-center">All Players</h2><br></br>
+                <div className="d-flex justify-content-center align-items-center container">
+                    <div className="align-self-center row row-cols-1 row-cols-md-3 g-4">
+                        {players.map(p =>
+                            <div key={p.id} className="col"> 
+                                <div className="card" style={{width: '18rem'}}>
+                                    <div className="card-body">
+                                        <h5 className="card-title">{p.name}</h5>
+                                        <button type="button" className="btn btn-primary"><Link className="link-light" to={`${p.id}/hits`}>See More Stats</Link></button>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>    
-                    )}
-                </div>   
+                            </div>    
+                        )}
+                    </div>   
+                </div>
         </div>
     )
 
